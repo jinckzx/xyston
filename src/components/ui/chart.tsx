906bf12461c9b2a2c260b4fe625535b1f0f76141
Anmol <89922277+jinckzx@users.noreply.github.com>
2024-11-20 16:18:55 +0530

    chart.tsx

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -1,14 +1,14 @@
-// import * as React from 'react';
-// import * as RechartsPrimitive from 'recharts';
-// import {
-//   NameType,
-//   Payload,
-//   ValueType,
-// } from 'recharts/types/component/DefaultTooltipContent';
+import React from 'react';
+import * as RechartsPrimitive from 'recharts';
+import {
+  NameType,
+  Payload,
+  ValueType,
+} from 'recharts/types/component/DefaultTooltipContent';
 
-// import { cn } from '@/lib/utils';
+import { cn } from '@/lib/utils';
 
-// Format: { THEME_NAME: CSS_SELECTOR }
+//Format: { THEME_NAME: CSS_SELECTOR }
 const THEMES = { light: '', dark: '.dark' } as const;
 
 export type ChartConfig = {
